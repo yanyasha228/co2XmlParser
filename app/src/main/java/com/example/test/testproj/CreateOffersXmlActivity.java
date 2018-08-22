124b87b6caa15d09a08da722d4d474b91fb33663
yanyasha228 <ruslan543213@gmail.com>
2018-08-22 16:29:20 +0300

    St 1 adding file attachment to mailSendIntent

--- app/src/main/java/com/example/test/testproj/CreateOffersXmlActivity.java
+++ app/src/main/java/com/example/test/testproj/CreateOffersXmlActivity.java
@@ -2,6 +2,7 @@ package com.example.test.testproj;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +13,11 @@ import com.example.test.testproj.adapters.DBAdapter;
 import com.example.test.testproj.helpers.CreateOfferXml;
 import com.example.test.testproj.models.Offer;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class CreateOffersXmlActivity extends AppCompatActivity implements View.OnClickListener {
@@ -38,26 +43,54 @@ public class CreateOffersXmlActivity extends AppCompatActivity implements View.O
 
     @Override
     public void onClick(View view) {
-        xmlStrToSend = new CreateOfferXml(favOffersList).createXml();
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, xmlStrToSend);
-        sendIntent.setType("text/plain");
-        startActivity(sendIntent);
-        finish();
-//        fileToSend = new CreateOfferXml(favOffersList,this).createXml();
+//        xmlStrToSend = new CreateOfferXml(favOffersList).createXml();
 //        Intent sendIntent = new Intent();
-//        Uri path = Uri.fromFile(fileToSend);
 //        sendIntent.setAction(Intent.ACTION_SEND);
-//        sendIntent.setType("vnd.android.cursor.dir/email");
-//        sendIntent.putExtra(Intent.EXTRA_STREAM, path);
+//        sendIntent.putExtra(Intent.EXTRA_TEXT, xmlStrToSend);
+//        sendIntent.setType("text/plain");
 //        startActivity(sendIntent);
 //        finish();
+
+        File fileToSend = CreateOfferXml.stringToFile(new CreateOfferXml(favOffersList).createXml() , this);
+        Intent sendIntent = new Intent();
+        Uri path = FileProvider.getUriForFile(this,  "com.example.test.testproj.fileprovider", fileToSend);
+        String fileStrtTest = null;
+        try {
+            fileStrtTest = getStringFromFile(fileToSend);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.setType("text/plain");
+        sendIntent.putExtra(Intent.EXTRA_STREAM, path);
+        startActivity(sendIntent);
+        finish();
+//        /data/user/0/com.example.test.testproj/files/co2ShopPriceListForRozetka.xml
     }
 
     private void getAllFavorites() {
         dbAdapter.open();
         favOffersList = dbAdapter.getOffers();
         dbAdapter.close();
+    }
+
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
+    public static String getStringFromFile (File file) throws Exception {
+        FileInputStream fin = new FileInputStream(file);
+        String ret = convertStreamToString(fin);
+        //Make sure you close all streams.
+        fin.close();
+        return ret;
     }
 }
